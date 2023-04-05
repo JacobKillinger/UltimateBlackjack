5fb174110bb195de874892e71c03a2a92fec9e21
Jacob Killinger <59901852+JacobKillinger@users.noreply.github.com>
2023-04-05 19:19:49 -0400

    Add files via upload

---
+++ Ultimate BlackJack/src/Main.java
@@ -0,0 +1,8 @@
+import java.beans.Visibility;
+
+public class Main {
+    public static void main(String[] args) {
+        BlackjackController controller = new BlackjackController(); //Creates controller and initiates the view from the controller
+        controller.startView();
+    }
+}
