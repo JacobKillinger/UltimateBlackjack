19401ee60405209c01d7929d48b851ce75175367
Jacob Killinger <59901852+JacobKillinger@users.noreply.github.com>
2023-04-05 19:20:47 -0400

    Delete Ultimate BlackJack/src directory

--- Ultimate BlackJack/src/Main.java
+++
@@ -1,8 +0,0 @@
-import java.beans.Visibility;
-
-public class Main {
-    public static void main(String[] args) {
-        BlackjackController controller = new BlackjackController(); //Creates controller and initiates the view from the controller
-        controller.startView();
-    }
-}
